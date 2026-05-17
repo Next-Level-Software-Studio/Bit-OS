a335d6c9f29d3d0cd5455b0a9a84f1d3603d836a
NextLevelSoftwareStudio <230233445+NextLevelSoftwareStudio@users.noreply.github.com>
2026-05-17 18:16:03 +0100

    .

--- installer.py
+++ installer.py
@@ -1,19 +1,36 @@
 import locale
 
 # lang = 'pt_PT', encoding = 'UTF-8'
-lang, encoding = locale.getdefaultlocale()
+encoding = locale.getencoding()
+lang, _ = locale.getlocale()
 portuguese = ["português", "portuguese", "portugiesisch"]
 english = ["inglês", "english", "englisch"]
 german = ["alemão", "german", "deutsch"]
 
 while True:
-    entrada = input("Digite algo: ")
+    if encoding != "UTF-8":
+        print("Your system's encoding is not UTF-8. Please change it to UTF-8 to continue.")
+        break
+    elif encoding == "UTF-8":
+        if lang == "pt_PT":
+            print("Idioma detectado: Português")
+            entrada = input("Que idioma deseja definir com padrão? ").lower()
+        elif lang == "en_US":
+            print("Language detected: English")
+            entrada = input("What language would you like to set as default? ").lower()
+        elif lang == "de_DE":
+            print("Sprache erkannt: Deutsch")
+            entrada = input("Welche Sprache möchten Sie als Standard festlegen? ").lower()
 
-    if entrada in portuguese:
-        print("Você disse oi ou olá!")
 
-    elif entrada in english:
-        print("You said bye or goodbye!")
+if entrada in portuguese:
+    print("Idioma definido para português!")
 
-    else:
-        print("Entrada não reconhecida.")
+elif entrada in english:
+    print("Language set to English!")
+
+elif entrada in german:
+    print("Sprache auf Deutsch eingestellt!")
+
+else:
+    print("Language not recognized or unavailable.")
