fe2b9a980970add853a9af6d080c1140e41607c1
NextLevelSoftwareStudio <230233445+NextLevelSoftwareStudio@users.noreply.github.com>
2026-05-23 18:40:27 +0100

    .

--- Files/amd64/FHS/System/Bit-OS Virtual File System Overlay Manager/bvfsom.sh
+++ Files/amd64/FHS/System/Bit-OS Virtual File System Overlay Manager/bvfsom.sh
@@ -1,2 +1,5 @@
+sudo mkdir -p "/System/Bit-OS Virtual File System Overlay Manager/Overlay"
 sudo system-python "/System/Bit-OS Virtual File System Overlay Manager/fuse.py" "/System/Bit-OS Virtual File System Overlay Manager/Overlay"
-sudo "/System/Bit-OS Virtual File System Overlay Manager/namespaces.py"
+sudo unshare --mount --pid --net --fork /bin/bash
+mount --make-rslave /
+chroot "/System/Bit-OS Virtual File System Overlay Manager/Overlay" /bin/bash
