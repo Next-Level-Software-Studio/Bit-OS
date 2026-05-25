ccecc63cf42c9bf4b07787e4e84fad4bdb45299c
NextLevelSoftwareStudio <230233445+NextLevelSoftwareStudio@users.noreply.github.com>
2026-05-25 17:30:32 +0100

    .

--- Files/amd64/System/Bit-OS Virtual File System Overlay Manager/bvfsom.sh
+++
@@ -1,4 +0,0 @@
-sudo mkdir -p "/System/Bit-OS Virtual File System Overlay Manager/Overlay"
-sudo python3 "/System/Bit-OS Virtual File System Overlay Manager/fuse.py" "/System/Bit-OS Virtual File System Overlay Manager/Overlay" &
-sleep 1
-sudo unshare --mount --pid --fork sh -c "mount --make-rslave / && chroot '/System/Bit-OS Virtual File System Overlay Manager/Overlay' /bin/bash"
